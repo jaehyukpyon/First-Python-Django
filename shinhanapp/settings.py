e73663dd67248e357ae20af53c81a663b90f3920
jaehyuk <jaehyuk.pyon@gmail.com>
2023-01-16 14:29:29 +0900

    settings.py에서 static 경로 설정

--- shinhanapp/settings.py
+++ shinhanapp/settings.py
@@ -118,6 +118,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_DIR = BASE_DIR / 'static'
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
